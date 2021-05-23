c8a8fdff4c9f225140ed7425794c1d4fffb38d7b
Katarzyna Bodanko <katarzyna.bodanko@gmail.com>
2021-05-23 15:53:40 +0200

    commit

--- js.js
+++ js.js
@@ -47,8 +47,8 @@ let articleList = [];
 let articleContent;
 let articlesContentWraper = document.createElement('div');
 articlesContentWraper.className = '.article_content_wraper';
-let mediaQuery = window.matchMedia('(min-width: 770px)');
-let menuMediaQuery = window.matchMedia('(max-width: 770px)');
+let mediaQuery = window.matchMedia('(max-width: 770px)');
+// let menuMediaQuery = window.matchMedia('(max-width: 770px)');
 
 let LoremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 localStorage.setItem("vOneLocalStorage", articleList);
@@ -67,17 +67,17 @@ eventListenerSet();
 function menuDropDown() {
     mobileArrowIsVisible === false ? mobileArrowIsVisible = true : mobileArrowIsVisible = false;
     console.log(mobileArrowIsVisible);
-    if (mobileArrowIsVisible) {
-        mobileMenu.style.top = '5.4rem';
-        mobileArrow.style.transform = 'rotate(0)';
-        // document.querySelector('.menu_option').style.opacity = '.99';
-    } else {
-        if (menuMediaQuery.matches) {
+    if (mediaQuery.matches) {
+        if (mobileArrowIsVisible) {
+            mobileMenu.style.top = '5.4rem';
+            mobileArrow.style.transform = 'rotate(0)';
+            // document.querySelector('.menu_option').style.opacity = '.99';
+        } else {
             mobileMenu.style.top = '-25rem';
             mobileArrow.style.transform = 'rotate(180deg)';
         }
+        console.log(mobileMenu.style.translateY);
     }
-    console.log(mobileMenu.style.translateY);
 }
 
 function vineyardVisibleArticleFade() {
@@ -145,7 +145,7 @@ function scrollToTop() {
 }
 
 function scrollToElement() {
-    mobileMenu.style.translate = '0 -30rem';
+    mobileMenu.style.top = '-25rem';
     mobileArrow.style.transform = 'rotate(180deg)';
     let scrollTargetNumber = (event.target.id.length > 0 ? event.target.id : event.target.parentElement.id).slice(2);
     console.log(scrollTargetNumber);
